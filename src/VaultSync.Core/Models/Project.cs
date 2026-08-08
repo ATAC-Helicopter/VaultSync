4effb3777d82ba8bc94357a28781574bf1c53735
Flavio Giacchetti <38221678+ATAC-Helicopter@users.noreply.github.com>
2026-08-08 13:01:57 +0200

    Persist explicit project folders

--- src/VaultSync.Core/Models/Project.cs
+++ src/VaultSync.Core/Models/Project.cs
@@ -3,17 +3,42 @@ namespace VaultSync.Core.Models;
 public record Project
 {
     public const string DestinationAllId = "__ALL__";
-    public int Id { get; init; }
+    public int Id
+    {
+        get; init;
+    }
     public string ExternalId { get; init; } = string.Empty;
-    public required string Name { get; init; }
-    public required string RootPath { get; init; }
-    public required string Preset { get; init; }
+    public required string Name
+    {
+        get; init;
+    }
+    public required string RootPath
+    {
+        get; init;
+    }
+    public required string Preset
+    {
+        get; init;
+    }
     public DateTime CreatedUtc { get; init; } = DateTime.UtcNow;
-    public bool NeedsRestore { get; init; }
-    public string? PreferredDestinationId { get; init; }
+    public bool NeedsRestore
+    {
+        get; init;
+    }
+    public string? PreferredDestinationId
+    {
+        get; init;
+    }
     public string EncryptionPolicy { get; init; } = ProjectEncryptionPolicy.Inherit;
-    public string? EncryptionKeyRef { get; init; }
+    public string? EncryptionKeyRef
+    {
+        get; init;
+    }
     public string RestoreMode { get; init; } = ProjectRestoreMode.Direct;
     public string VerificationPolicy { get; init; } = ProjectVerificationPolicy.Always;
     public string Tags { get; init; } = string.Empty;
+    public string? GroupId
+    {
+        get; init;
+    }
 }
